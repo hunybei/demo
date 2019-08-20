ac876521c64a8e60d09e66d0bad1cb21377cf771
wzn <November002@163.com>
2019-08-20 15:34:33 +0800

    Initial commit

---
+++ instance/src/main/java/com/test/instance/InstanceApplication.java
@@ -0,0 +1,22 @@
+package com.test.instance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class InstanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstanceApplication.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "demo 下的 instance";
+    }
+}
